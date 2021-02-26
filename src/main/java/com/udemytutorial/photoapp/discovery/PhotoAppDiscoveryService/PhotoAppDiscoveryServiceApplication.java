d3992c31cf70d75bb64c23231d0ddcf32fed0c95
Rupesh <rupeshkumar9312@gmail.com>
2021-02-25 21:55:01 -0800

    Set up eureka service

--- src/main/java/com/udemytutorial/photoapp/discovery/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplication.java
+++ src/main/java/com/udemytutorial/photoapp/discovery/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplication.java
@@ -2,12 +2,14 @@ package com.udemytutorial.photoapp.discovery.PhotoAppDiscoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PhotoAppDiscoveryServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
+    }
 
 }
